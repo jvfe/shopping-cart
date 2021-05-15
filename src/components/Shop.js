23d560165ec3003baf8efee36b5e299d7cce1965
jvfe <jvfecav@gmail.com>
2021-05-15 14:05:55 -0300

    feat: Style shop page

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,17 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { SimpleGrid } from "@chakra-ui/react";
+import { Flex, Wrap, Heading } from "@chakra-ui/react";
 import ShopItem from "./ShopItem";
 
 const Shop = ({ items }) => {
   return (
-    <div>
-      <SimpleGrid columns={2} spacingX="2em" spacingY="2em" px={6} py={4}>
+    <Flex
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      bgColor="black"
+    >
+      <Heading color="white" mt={2}>
+        Our Products
+      </Heading>
+      <Wrap
+        w={{ sm: "100%", lg: "90%" }}
+        alignItems="center"
+        justifyContent="center"
+        mt={2}
+        p={4}
+      >
         {items.map((item) => {
-          return <ShopItem item={item} />;
+          return <ShopItem key={item.id} item={item} />;
         })}
-      </SimpleGrid>
-    </div>
+      </Wrap>
+    </Flex>
   );
 };
 
--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,12 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Box, Image, Heading, Text, Button } from "@chakra-ui/react";
 
 const ShopItem = ({ item }) => {
   return (
-    <div key={item.id} className="shop-item">
-      <Link to={`/shop/${item.id}`}>{item.name}</Link>
-      <img src={item.photo} alt={item.name} className="shop-image" />
-    </div>
+    <Box
+      maxW={{ lg: "sm", md: "40%", sm: "30%" }}
+      maxH="sm"
+      color="white"
+      borderRadius="lg"
+      borderWidth="1px"
+      overflow="hidden"
+      as={Link}
+      to={`/shop/${item.id}`}
+    >
+      <Image
+        h="80%"
+        w="100%"
+        objectFit="cover"
+        src={item.photo}
+        alt={item.name}
+      />
+      <Box px={3} py={3}>
+        <Heading
+          borderBottom="1px"
+          lineHeight={1}
+          fontSize={{ sm: "lg", md: "3xl" }}
+        >
+          {item.name}
+        </Heading>
+        <Text mt={1} fontSize="lg">{`$${item.price}`}</Text>
+      </Box>
+    </Box>
   );
 };
 
